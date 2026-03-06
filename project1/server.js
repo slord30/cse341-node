4986e92bdf4c5ccc01432e131b464d1b4d084558
Sarah <sarahlord79@gmail.com>
2026-03-06 16:27:09 -0600

    first commit

---
+++ project1/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('/data/database');
+const contactsRoutes = require('./routes/contacts');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+app.use('/contacts', contactsRoutes);
+
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port);
+        console.log(`Connected to DB and listening on ${port}`);
+    }
+});
